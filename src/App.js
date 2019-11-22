665d9f23d5d0d826175158824b5ed1cc29e971bf
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2019-11-21 22:43:23 -0300

    added login css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import './App.css';
+import Login from './Login/index'
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Login></Login>
+    </>
   );
 }
 
---
+++ src/Login/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import './styles.css';
+
+export default function Login(){
+
+    return (
+        <>
+        <div className="loginImageContainer">
+      
+            <div className="loginContainer">
+                <form>
+                  
+                  <label htmlFor="email">E-mail</label>
+                  <input
+                    id="email"
+                    type="email"
+                    placeholder="placeholder@foda.com"
+                  />
+
+                  <label htmlFor="senha">Senha</label>
+                  <input
+                    id="senha"
+                    type="password"
+                    placeholder="daapatinhah123"
+                  />
+                  <div className="loginOptions">
+                    <button type="submit">Registrar</button>
+                    <button type="submit">Entrar</button>
+                  </div>
+                </form>
+            </div>
+        </div>
+        </>
+    )
+
+}
