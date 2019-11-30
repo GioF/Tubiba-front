6a53b6c888357b84921f3ce05a2c4273e2dc789c
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2019-11-30 16:13:11 -0300

    using router for simpler css testing

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 
-import { MdLibraryBooks, MdDashboard, MdPowerSettingsNew, MdVpnKey } from 'react-icons/md';
 
-import { Container, Sidebar, Content } from './styles';
+import { Container, Content } from './styles';
+import Sidebar from '../../components/Sidebar';
 import Routes from './routes';
-
-//TEMP
-//import Exercises from '../../components/Exercises';
-//import ExerciseForm from '../../components/ExerciseForm';
-import ExerciseList from '../../components/ExerciseList';
 
 export default function Dashboard() {
   return (
     <Container>
-      <Sidebar>
-        <button> <MdDashboard         size={40} color="#fff" /> </button>
-        <button> <MdLibraryBooks      size={40} color="#fff" /> </button>
-        <button> <MdVpnKey            size={40} color="#fff" /> </button>
-        <button> <MdPowerSettingsNew  size={40} color="#fff" /> </button>
-      </Sidebar>
+      <Sidebar/>
       <Content>
-        <ExerciseList></ExerciseList>
         <Routes/>
       </Content>
     </Container>
--- src/pages/Dashboard/routes.js
+++ src/pages/Dashboard/routes.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
+import Statistics from '../../components/Statistics';
+// import  from '../../components/'
+// import  from '../../components/'
+// import  from '../../components/'
+// import  from '../../components/'
+
 export default function Routes(){
   return (
     <Switch>
-      <Route path="/dashboard/exercises" component="" />
+      <Route path="/dashboard/statistics" component={Statistics} />
       <Route />
       <Route />
     </Switch>
