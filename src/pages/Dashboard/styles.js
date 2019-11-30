0c3d91ebff7b3b13d76e8d295037e2d1ea14da60
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2019-11-30 15:58:09 -0300

    removing unnecessary css

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -7,41 +7,6 @@ export const Container = styled.div`
   height: 100%;
 `;
 
-export const Sidebar = styled.div`
-
-  background-color: #01D088;
-
-  width: 90px;
-  min-height: 100%;
-
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  float: left;
-
-  button {
-    width: 65px;
-    height: 65px;
-  
-    border-radius: 16px;
-    border: 0px;
-  
-    margin-top: 8px;
-  
-    cursor: pointer;
-  
-    background: rgba(0, 172, 112, 0.5);
-  }
-
-  button:hover {
-    background: rgba(35, 231, 162, 0.5);
-    transition-duration: 200ms;
-  }
-
-`;
-
 export const Content = styled.div `
   padding: 20px 40px;
 
@@ -59,8 +24,9 @@ export const Content = styled.div `
   display: grid;
   grid-gap: 20px;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 2fr;
-  grid-template-areas: "st gr"
+  grid-template-rows: 1fr 3fr 4fr;
+  grid-template-areas: "tit tit"
+                       "st gr"
                        "ex ex"
 
 `;
