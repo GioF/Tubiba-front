be642ae7fa9bc61d6329e6f16ae56f2527d54bd1
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2019-12-02 16:34:17 -0300

    done styling the solvedExercise component

--- src/components/SolvedExercise/index.js
+++ src/components/SolvedExercise/index.js
@@ -1,46 +1,62 @@
 import React from 'react';
+import { useState } from 'react';
 
-import { Container, Correto, Parcial, Incorreto, Answer, Options } from './styles';
+import { Container, Answer, Options, Overlay, Fill, Justificatory, Grading} from './styles';
 
-export default function SolvedExercise() {
+export default function SolvedExercise(props) {
 
-  const props = {
-    data: {
-      question: "teste",
-      student: "vinegar doppio",
-      command: "is cioccolata dumb?",
-      answer: "yes"
-    }
+  const [displayJustificatory, setDisplayJustificatory] = useState(false);
+  const [gradingBackground, setGradingBackground] = useState('');
+
+  const colors = [
+    '#54FA8C',
+    '#FFC36A',
+    '#FE5C5C'
+  ]
+
+  
+  function changeJustificatory(){
+    setDisplayJustificatory(!displayJustificatory);
+  };
+
+  function handleGrading(background){
+    changeJustificatory();
+    setGradingBackground(colors[background]);
   }
 
   return (
-    <Container>
-      <h1>
-        {props.data.question}
-      </h1>
+    <Overlay>
+      <Container>
+        <h1>
+          {props.data.question ? props.data.question : "teste"}
+        </h1>
 
-      <h2>
-        <bold>Aluno</bold> {props.data.student}
-      </h2>
+        <h2>
+          <bold>Aluno</bold> {props.data.student ? props.data.student : "vinegar doppio"}
+        </h2>
 
-      <h3>
-        {props.data.command}
-      </h3>
+        <h3>
+          {props.data.command ? props.data.command : "is cioccolata dumb?"}
+        </h3>
 
-      <label>Resposta:</label>
-      <Answer>
-        <p>
-          {props.data.answer}
-        </p>
-      </Answer>
+        <label>Resposta:</label>
+        <Answer>
+          <p>
+            {props.data.answer ? props.data.answer : "yes"}
+          </p>
+        </Answer>
 
-      <p><label>Correção:</label></p>
-      
-      <Options>
-        <Correto>Correto</Correto>
-        <Parcial>Parcial</Parcial>
-        <Incorreto>Incorreto</Incorreto>
-      </Options>
-    </Container>
+        <Fill>
+          <p><label>Correção:</label></p>
+        
+          <Options>
+            <Justificatory  display={displayJustificatory ? 'visible' : 'hidden'} background={gradingBackground}/>
+            <Grading        display={displayJustificatory ? 'hidden' : 'visible'} onClick="" btncolor={colors[0]}>Correto</Grading>
+            <Grading        display={displayJustificatory ? 'hidden' : 'visible'} onClick={() => handleGrading(1)}  btncolor={colors[1]}>Parcial</Grading>
+            <Grading        display={displayJustificatory ? 'hidden' : 'visible'} onClick={() => handleGrading(2)} btncolor={colors[2]}>Incorreto</Grading>
+          </Options>
+        </Fill>
+      </Container>
+    </Overlay>
   );
 }
--- src/components/SolvedExercise/styles.js
+++ src/components/SolvedExercise/styles.js
@@ -1,14 +1,39 @@
 import styled from 'styled-components';
 
+export const Overlay = styled.div`
+  background-color: #01D08870;
+  
+  position: fixed;
+
+  height: 100%;
+  width: 100%;
+
+  z-index: 2000;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  backdrop-filter: blur(4px);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+
   height: 90%;
   width: 50%;
   
-  margin: auto;
+  margin: auto auto;
 
-  padding: 70px 40px 20px 40px;
+  padding: 10vh 40px 20px 40px;
 
-  background-color: goldenrod;
+  background-color: #fff;
   
   border-radius: 20px;
 
@@ -29,6 +54,7 @@ export const Container = styled.div`
 
   label {
     font-weight: bold;
+    font-size: 18px;
   }
 
   bold {
@@ -41,22 +67,60 @@ export const Container = styled.div`
 export const Answer = styled.div`
   margin: 20px 0px;
   width: 100%;
+  height: 100%;
 `;
 
 export const Options = styled.div`
   display: flex;
   flex-direction: column;
   float: left;
+
+  margin-top: auto;
+
+  position: relative;
+
+  width: 100%;
+  
+
+  button {
+    margin-top: 10px;
+    height: 42px;
+    width: 120px;
+    color: #000;
+  }
 `;
 
-export const Correto = styled.button`
-  margin: auto;
+export const Grading = styled.button`
+  background-color: ${props => props.btncolor};
+
+  visibility: ${props => props.display};
+
+  &:hover {
+    background-color: ${props => props.btncolor + 'A5'};
+    transition-duration: 300ms;
+  }
+
 `;
 
-export const Parcial = styled.button`
-  margin: auto;
+export const Justificatory = styled.textarea`
+  background-color: ${props => props.background};
+
+  position: absolute;
+  visibility: ${props => props.display};
+
+  border-radius: 12px;
+  border: 0px;
+
+  padding: 20px;
+
+  height:100%;
+  width: 100%;
+
+  resize: none;
+
 `;
 
-export const Incorreto = styled.button`
-  margin: auto;
+export const Fill = styled.div`
+  width: 100%;
+  height: 100%;
 `;
